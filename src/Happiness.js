97d3e818045bcf752f5c01560466953602967f82
Alexina Coullandreau <mozart38@gmail.com>
2020-11-29 15:23:22 +0100

    Effect "Bonheur simple"

---
+++ src/Happiness.js
@@ -0,0 +1,109 @@
+import Effect from './Effect'
+import Uniform from './Uniform'
+
+class TestEffect extends Effect {
+    constructor(scene) {
+        super();
+        this.scene = scene;
+        this.uniforms['temperature'] = new Uniform(20, 0, 50, 0.1);
+        this.uniforms['humidity'] = new Uniform(60, 0, 100, 0.1);
+        this.uniforms['pressure'] = new Uniform(1012, 980, 1025, 0.1);
+        this.uniforms['light'] = new Uniform(160, 0, 750, 1.0);
+        this.uniforms['hour'] = new Uniform(15, 0, 23, 0.1);
+        this.uniforms['speed'] = new Uniform(10, 1, 100, 1);
+        this.uniforms['fft'] = new Uniform([]);
+
+        var layers = 5;
+        var y = 200 / (layers - 2) / 2;
+        var deltaY = 200 / layers;
+
+        // 6
+        this.scene.addSpacedPixelSamples(y, 6);
+        y += deltaY;
+
+        // 8
+        this.scene.addSpacedPixelSamples(y, 8);
+        y += deltaY;
+
+        // 8
+        this.scene.addSpacedPixelSamples(y, 8);
+        y += deltaY;
+
+        // 6
+        this.scene.addSpacedPixelSamples(y, 6);
+        y += deltaY;
+
+        //initial base colours
+        this.colors = [
+            [235, 102, 98],
+            [230, 133, 171],
+            [237, 171, 208],
+            [247, 177, 114],
+            [247, 211, 126],
+            [168, 200, 154],
+            [130, 200, 129],
+            [32, 143, 148],
+            [32, 61, 133]
+        ];
+
+        this.currentPulse = 0;
+        this.previousPulse = 0;
+        this.colorArrayMax = 1;
+        this.colorsRange = [this.colors[0], this.colors[1]];
+
+        this.RGB = this.colors[0];
+        this.modulo = 1000;
+        this.initialTime = this.uniforms['time'].value;
+        this.timeSpeed = 0.0;
+        // fixed speed for this effect
+        this.speed = 70;
+    }
+
+    getBoundaries = () => {
+        if (this.currentPulse !== this.previousPulse) {
+            var random = Math.floor(Math.random() * this.colors.length);
+            if (random === this.colorArrayMax) {
+                if (random === this.colors.length - 1) {
+                    random = 0;
+                } else {
+                    random += 1;
+                }
+            }
+
+            this.colorsRange= [this.colors[this.colorArrayMax], this.colors[random]];
+            this.colorArrayMax = random;
+            this.previousPulse = this.currentPulse;
+        };
+
+    }
+
+    preprocess() {
+        var currentTime = this.uniforms['time'].value;
+
+        if (currentTime !== undefined) {
+            // compute color for this frame
+            this.RGB = this.getRGB();
+
+            // update time counter
+            var deltaTime = currentTime - this.initialTime;
+            this.initialTime = currentTime;
+            this.timeSpeed += deltaTime * this.speed/70000;
+
+            // update currentIndex and ranges
+            this.currentPulse = Math.floor((this.timeSpeed) % (this.colors.length));
+            this.getBoundaries();
+        } 
+        else {
+            this.RGB = this.colors[this.currentPulse];
+        }
+    }
+
+
+    renderPixel(x, y) {
+        var color = this.RGB;
+
+        return color;
+    }
+}
+
+export default TestEffect;
