8e03856cfc835b590c6d6d4ab22f657cf5ead539
Alexina Coullandreau <mozart38@gmail.com>
2020-11-29 16:44:29 +0100

    Effect "Automne"

---
+++ src/Autumn.js
@@ -0,0 +1,102 @@
+import Effect from './Effect'
+import Uniform from './Uniform'
+
+class TestEffect extends Effect {
+    constructor(scene) {
+        super();
+        this.scene = scene;
+        this.uniforms['temperature'] = new Uniform(20, 0, 50, 0.1);
+        this.uniforms['humidity'] = new Uniform(60, 0, 100, 0.1);
+        this.uniforms['pressure'] = new Uniform(1012, 980, 1025, 0.1);
+        this.uniforms['light'] = new Uniform(160, 0, 750, 1.0);
+        this.uniforms['hour'] = new Uniform(15, 0, 23, 0.1);
+        this.uniforms['speed'] = new Uniform(10, 1, 100, 1);
+        this.uniforms['fft'] = new Uniform([]);
+
+        var layers = 5;
+        var y = 200 / (layers - 2) / 2;
+        var deltaY = 200 / layers;
+
+        // 6
+        this.scene.addSpacedPixelSamples(y, 6);
+        y += deltaY;
+
+        // 8
+        this.scene.addSpacedPixelSamples(y, 8);
+        y += deltaY;
+
+        // 8
+        this.scene.addSpacedPixelSamples(y, 8);
+        y += deltaY;
+
+        // 6
+        this.scene.addSpacedPixelSamples(y, 6);
+        y += deltaY;
+
+        //initial base colours
+        this.colors = [
+            [97, 64, 78],
+            [195, 95, 95],
+            [226, 145, 50],
+            [191, 154, 115],
+            [224, 196, 137],
+            [167, 167, 98],
+            [131, 117, 25],
+            [197, 104, 23],
+            [59, 30, 16]
+        ];
+
+        this.currentIndex = 0;
+        this.colorsRange = [this.colors[0], this.colors[1]];
+        this.modulo = 1000;
+        this.initialTime = this.uniforms['time'].value;
+        this.timeSpeed = 0.0;
+        this.ranges = this.getBoundaries();
+        // fixed speed for this effect
+        this.speed = 9;
+    }
+
+    getBoundaries = () => {
+        var colors;
+
+        if (this.currentIndex === 0) {
+            colors = [this.colors[this.colors.length - 1], this.colors[this.currentIndex]];
+        } else {
+            colors = [this.colors[this.currentIndex - 1], this.colors[this.currentIndex]];
+        }
+
+        this.colorsRange = colors;
+
+    }
+
+
+    preprocess() {
+        var currentTime = this.uniforms['time'].value;
+
+        if (currentTime !== undefined) {
+            // compute color for this frame
+            this.RGB = this.getRGB();
+
+            // update time counter
+            var deltaTime = currentTime - this.initialTime;
+            this.initialTime = currentTime;
+            this.timeSpeed += deltaTime * this.speed/50000;
+
+            // update currentIndex and ranges
+            this.currentIndex = Math.floor((this.timeSpeed) % (this.colors.length));
+            this.getBoundaries();
+        } 
+        else {
+            this.RGB = this.colors[this.currentIndex];
+        }
+    }
+
+
+    renderPixel(x, y) {
+        var color = this.RGB;
+
+        return color;
+    }
+}
+
+export default TestEffect;
